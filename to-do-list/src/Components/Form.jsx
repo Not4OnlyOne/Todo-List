35ae20fc86f342be7bfd7c856cba283c8f0e71a6
SikindarMirza <sikindar247@gmail.com>
2021-01-06 23:06:49 +0530

    Add item

--- to-do-list/src/Components/Form.jsx
+++ to-do-list/src/Components/Form.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 function Todo() {
-  function inputHandler(e) {
-    //let inputValue = [];
-    let array = [];
-    //console.log("event type:" + e.type);
-    let inputValue = document.querySelector('input[type="text"]');
-    for (let i = 0; inputValue > i; ) array.push(inputValue.value);
-    array.push(inputValue.id);
-    array.push(inputValue.placeholder);
-    console.log(array.length);
-    // for (let i = array.length; i > 0; i--) {
-    //   console.log(array.pop(i));
+  const [inputValue, setInputvalue] = useState('')
+  const [list, setList] = useState([])
+  const inputHandler = (e) => {
+    debugger
+    const newList = [...list];
+  // insted of plian strings pushing, push objcts with id and value property
+
+
+    newList.push(inputValue);
+    setList(newList)
+    setInputvalue('')
+    
+ 
   }
 
+  const changeHandler = (e) => {
+    setInputvalue(e.target.value)
+  }
+ 
+
   return (
+
     <div>
       {/* <label for="name">Enter </label> */}
-      <input type="text" id="input1" placeholder="enter" />
-      <button onClick={inputHandler} type="submit">
+      <input id="input1" placeholder="enter" value={inputValue} onChange={changeHandler}/>
+      <button onClick={inputHandler}>
         ADD
       </button>
+     
+    {list.length>0 && list.map((item) => {
+      return (
+        <div>
+        <h2>{item}</h2>
+        </div>
+      )
+    })}
     </div>
   );
 }
